f97baab1f09717bc82a4019384be1254dcda7df7
NaserUddinMahmud <mahmudbappy01@gmail.com>
2023-03-28 03:11:56 +0600

    shop folder location changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './components/Header/Header'
-import Shop from './components/Header/Shop/Shop'
+import Shop from './components/Shop/Shop'
 
 function App() {
   const [count, setCount] = useState(0)
