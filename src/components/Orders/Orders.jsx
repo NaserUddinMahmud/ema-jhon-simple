bb9095ba88846db7656e96e51fce3c335e445221
NaserUddinMahmud <mahmudbappy01@gmail.com>
2023-04-09 05:48:17 +0600

    review order & proceed chackout added

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css'
-import { removeFromDb } from '../../utilities/fakedb';
+import { deleteShoppingCart, removeFromDb } from '../../utilities/fakedb';
 
 
 const Orders = () => {
     const savedCart =useLoaderData();
     const [cart, setCart] = useState(savedCart)
+    console.log(cart);
     const handleRemoveFromCart = (id) =>{
         const remaining = cart.filter(pd => pd.id !== id);
         setCart(remaining);
         removeFromDb(id);
     }
 
-    console.log(savedCart);
+    const handleClearCart = () => {
+        setCart([])
+        deleteShoppingCart()
+    }
+
+    // console.log(savedCart);
     return (
         <div className='shop-container'>
             <div className='review-container'>
@@ -30,7 +36,12 @@ const Orders = () => {
             <div>
                 <Cart
                 cart={cart}
-                ></Cart>
+                handleClearCart={handleClearCart}
+                >
+                   <Link className='proceed-link' to="/checkout">
+                    <button className='btn-proceed'>Proceed Checkout</button>
+                   </Link>
+                </Cart>
             </div>
         </div>
     );
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { addToDb, getShoppingCart } from "../../utilities/fakedb";
+import {
+  addToDb,
+  deleteShoppingCart,
+  getShoppingCart,
+} from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
+import { Link } from "react-router-dom";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("products.json")
@@ -14,15 +19,15 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
-  useEffect( () =>{
+  useEffect(() => {
     const storedCart = getShoppingCart();
-    const savedCart =[];
+    const savedCart = [];
     // step 1: get id
-    for (const id in storedCart){
+    for (const id in storedCart) {
       // step 2: get the product by using id
-      const addedProduct = products.find(product => product.id === id)
+      const addedProduct = products.find((product) => product.id === id);
       console.log(addedProduct);
-      if (addedProduct){
+      if (addedProduct) {
         // step 3: get and set quantity of the product
         const quantity = storedCart[id];
         addedProduct.quantity = quantity;
@@ -32,38 +37,47 @@ const Shop = () => {
     }
     // step 5: set the cart
     setCart(savedCart);
-  }, [products])
+  }, [products]);
 
   const handleAddToCart = (product) => {
     // cart.push(product);
     // const newCart = [...cart, product];
     let newCart = [];
-    const exists = cart.find(pd => pd.id === product.id);
-    if(!exists){
+    const exists = cart.find((pd) => pd.id === product.id);
+    if (!exists) {
       product.quantity = 1;
       newCart = [...cart, product];
-    }else{
-      exists.quantity = exists.quantity + 1; 
-      const remaining = cart.filter(pd => pd.id !== product.id);
+    } else {
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter((pd) => pd.id !== product.id);
       newCart = [...remaining, exists];
     }
     setCart(newCart);
-    addToDb(product.id)
-  }
+    addToDb(product.id);
+  };
+
+  const handleClearCart = () => {
+    setCart([]);
+    deleteShoppingCart();
+  };
 
   return (
     <div className="shop-container">
       <div className="products-container">
         {products.map((product) => (
-          <Product 
-            key={product.id} 
+          <Product
+            key={product.id}
             product={product}
-            handleAddToCart = {handleAddToCart}
+            handleAddToCart={handleAddToCart}
           ></Product>
         ))}
       </div>
       <div className="cart-container">
-        <Cart cart = {cart}></Cart>
+        <Cart cart={cart} handleClearCart={handleClearCart}>
+          <Link className="proceed-link" to='/orders'>
+            <button className='btn-proceed'>Review Order</button>
+          </Link>
+        </Cart>
       </div>
     </div>
   );
